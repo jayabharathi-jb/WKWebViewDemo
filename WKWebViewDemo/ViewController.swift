f15c517c3698824b77fb9b9f306895e67de1996d
jayabharathi-jb <jayabharathi.s@thoughtworks.com>
2022-04-22 12:30:09 +0530

    Initial Commit

---
+++ WKWebViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WKWebViewDemo
+//
+//  Created by Jayabharathi S on 22/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
